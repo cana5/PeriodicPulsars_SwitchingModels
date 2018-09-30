5b2073b1e9240b702a0445484193f01b199f6e19
Chan <41718448+cana5@users.noreply.github.com>
2018-09-30 15:44:01 +1000

    Update PSR-B1828-11_switching_constant_period.py

--- PSR-B1828-11_switching_constant_period.py
+++ PSR-B1828-11_switching_constant_period.py
@@ -46,7 +46,8 @@ plt.tight_layout()
 plt.savefig('precession_data.pdf')
 
 # Fill in the function to describe precession:
-def SignalModel(time, nudot1, nudot2, nuddot, T, tAB, tBC, tCD, phi0, sigma):
+def SignalModel(time, nudot1, nudot2, nuddot, T, tAB, tBC, tCD, phi0, sigma,
+               **kwargs):
 
     # This is the independent variable minus the MJD value:
     time = time - 49621 * 86400
@@ -147,3 +148,21 @@ result = tupak.run_sampler(
     likelihood=likelihood, priors=priors, sampler='dynesty', npoints= 100,
     walks=10, outdir=outdir, label=label, clean=True)
 result.plot_corner()
+
+# Define a new plot:
+fig, ax = plt.subplots()
+
+# Run values from 'result.posterior' and fit the data:
+for i in range(4000):
+    sample_dictionary = result.posterior.sample().to_dict('records')[0]
+    sample_dictionary.update(fixed_priors)
+    ax.plot(MJD_seconds, SignalModel(MJD_seconds, **sample_dictionary), 
+            color='gray', alpha=0.05)
+
+# Plot the fitted data:
+ax.scatter(MJD_seconds, nudot, marker='.')
+txt = ("Graph of the changing period switching function with multiple data "
+       "fitted parameters, and the pulsar's data points.")
+plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', 
+            fontsize=10)
+fig.savefig('{}/data_with_fit.pdf'.format(outdir))
